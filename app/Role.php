5a0de649afedc1995285ee3eae2f234539c3591b
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-11-06 18:02:53 -0300

    implementación de Roles

---
+++ app/Role.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LABQUIAM;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    //
+
+    public function users(){
+    return $this
+        ->belongsToMany('LABQUIAM\User')
+        ->withTimestamps();
+}
+}
