9f573968c39355f4fd0b5034d65c1f58497bae56
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-11-13 12:11:04 -0300

    paneles de inicio

--- resources/views/usuario/inicio.blade.php
+++ resources/views/usuario/inicio.blade.php
@@ -2,6 +2,35 @@
 @extends('layouts.app')
 @section('content')
 
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-center">Panel de Usuario</div>
 
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    @if(Auth::user()->hasRole('admin'))
+                       
+                         <div align="center">
+                            {{-- <a  rol="button" href="{{url('admin/usuario')}}" class="btn btn-outline-primary btn-sm " > <i class="fa fa-plus"></i> Nuevo Técnico</a>
+                            <a  rol="button" href="{{url('admin/listado/usuario')}}" class="btn btn-outline-info btn-sm " > <i class="fa fa-plus"></i> Lista de Técnico</a>
+                            <a  rol="button" href="{{url('admin/listado/cliente')}}" class="btn btn-outline-success btn-sm " > <i class="fa fa-plus"></i> Lista de Clientes</a> --}}
+                            {{-- <a  rol="button" href="{{url('producto/agregar')}}" class="btn btn-outline-warning btn-sm " > <i class="fa fa-plus"></i> Nuevo</a> --}}
+                        </div>
+                    
+                    @endif
+                    
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
