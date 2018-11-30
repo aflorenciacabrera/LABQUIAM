48169465ac37d376cce9f041b12e87bc72ac2794
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-11-30 16:38:41 -0300

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,4 @@
 
-
 {{-- ********************************** Importación de Bootstrap ******************************************** --}}
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
