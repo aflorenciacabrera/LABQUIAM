ff7ae1e8431b2544ee7c1d07986bae7c3103632b
Florencia Cabrera <aflorenciacabrera@gmail.com>
2018-12-03 09:43:07 -0300

    inicio

--- resources/views/admin/inicio.blade.php
+++ resources/views/admin/inicio.blade.php
@@ -4,28 +4,14 @@
 
 @section('content')
 
-               {{-- Mensajes de Alerta --}}
-<div class="container">
-    <div class="row">
-        <div class="text-center col-md-12 ">
-           
-       </div>
-    </div>
+             
 
-{{-- Panel de lista de Representantes registrados       --}}
-<div class="row">
-    <div class="col-md-12 col-md-offset-">
-        <div class="panel-body">
-            {{-- Titulo --}}
-            <div class="pull-left"><h3>Lista de Usuarios</h3></div>
-            {{-- contenido de la tabla --}}
-           
+            <div class="row justify-content-center">
+               
+                     <img src="{{asset('img/icono quimica.png')}}" width="300" />
+                    
             </div>
-        </div>
-    </div>
-</div>
-
-
+        
 @endsection
 
 @extends('layouts.app')
